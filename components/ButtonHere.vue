3f9876678dc2d2c3581c3ee3ffee0659a121d25b
Shin <penspulse@gmail.com>
2024-11-06 13:40:31 +0800

    first commit

---
+++ components/ButtonHere.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const name = ref<string>('123');
+</script>
+
+<template>
+  <div>{{ name }}</div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>首頁</h1>
+    <ButtonHere />
+  </div>
+</template>
+
+<style scoped></style>
